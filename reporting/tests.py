d969ac6fff7b28f74db5fe9ddecd22f5fbc3165a
vuxuantung <toilatung90@gmail.com>
2012-02-05 17:26:34 +0700

    test for extract_information function

--- reporting/tests.py
+++ reporting/tests.py
@@ -1,14 +1,7 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from reporting.models import Upload, Spreadsheet_report
 from datetime import datetime
-from extract_information import get_list_of_object
+from extract_information import get_list_of_object, extract_information
 
 
 class SimpleTest(TestCase):
@@ -29,6 +22,88 @@ class SimpleTest(TestCase):
     def test_get_list_of_object(self):
         #test if the function get_list_of_object is correct
         message, objects_list = get_list_of_object('Upload.objects.all()', [(1,2)])
-        self.assertEqual(message, 'ok')
+        self.assertEqual(message, 'ok') #check if the returned message is 'ok;
+
+        #check if the returned objects list is the correct list
         self.assertEqual(objects_list[0],self.upload)
         self.assertEqual(len(objects_list),1)
+
+    def test_extract_information_function(self):
+        index_of_function = []
+        index_of_head = []
+        body = []
+        indexes_of_body = []
+        index_of_excel_function = []
+        excel_function = []
+        other_info = []
+        index_of_other_info = []
+
+        #call function to test function_name extraction
+        function_name, head = extract_information(index_of_function, index_of_head, body, indexes_of_body,
+                        index_of_excel_function, excel_function, '#<function()>', 1, 2, other_info, index_of_other_info)
+        self.assertEqual(function_name, 'function()') #test if the function_name is extracted correctly
+        self.assertEqual(head, '') #test if the head is extracted correctly
+        self.assertEqual(index_of_function, [(1,2)]) #test if the the index of function_name is assigned correctly
+        self.assertEqual(index_of_head,[]) #the index of head should be empty
+        self.assertEqual(body, []) #the body should be empty
+        self.assertEqual(indexes_of_body, []) #the index of body should be empty
+        self.assertEqual(index_of_excel_function, []) #the index of excel function should be empty
+        self.assertEqual(excel_function, []) #the excel function list should be empty
+        self.assertEqual(other_info, []) #other information should be empty
+        self.assertEqual(index_of_other_info, []) #index of other information should be empty
+
+        #test for head extraction
+        function_name, head = extract_information(index_of_function, index_of_head, body, indexes_of_body,
+                        index_of_excel_function, excel_function, '{{head:head}}', 1, 2, other_info, index_of_other_info)
+        self.assertEqual(function_name, '') #test if the function_name is extracted correctly
+        self.assertEqual(head, 'head') #test if the head is extracted correctly
+        self.assertEqual(index_of_function, [(1,2)]) #test if the the index of function_name is assigned correctly
+        self.assertEqual(index_of_head,[(1,2)]) #the index of head should be [(1,2)]
+        self.assertEqual(body, []) #the body should be empty
+        self.assertEqual(indexes_of_body, []) #the index of body should be empty
+        self.assertEqual(index_of_excel_function, []) #the index of excel function should be empty
+        self.assertEqual(excel_function, []) #the excel function list should be empty
+        self.assertEqual(other_info, []) #other information should be empty
+        self.assertEqual(index_of_other_info, []) #index of other information should be empty
+
+        #test for body extraction
+        function_name, head = extract_information(index_of_function, index_of_head, body, indexes_of_body,
+                        index_of_excel_function, excel_function, '{{body:body}}', 1, 2, other_info, index_of_other_info)
+        self.assertEqual(function_name, '') #test if the function_name is extracted correctly
+        self.assertEqual(head, '') #test if the head is extracted correctly
+        self.assertEqual(index_of_function, [(1,2)]) #test if the the index of function_name is assigned correctly
+        self.assertEqual(index_of_head,[(1,2)]) #the index of head should be [(1,2)]
+        self.assertEqual(body, ['body']) #the body should be ['body']
+        self.assertEqual(indexes_of_body, [(1,2)]) #the index of body should be empty
+        self.assertEqual(index_of_excel_function, []) #the index of excel function should be empty
+        self.assertEqual(excel_function, []) #the excel function list should be empty
+        self.assertEqual(other_info, []) #other information should be empty
+        self.assertEqual(index_of_other_info, []) #index of other information should be empty
+
+        #test for excel_function extraction
+        function_name, head = extract_information(index_of_function, index_of_head, body, indexes_of_body,
+                        index_of_excel_function, excel_function, ':= "{{body:body2}}" + "tung"', 1, 2, other_info, index_of_other_info)
+        self.assertEqual(function_name, '') #test if the function_name is extracted correctly
+        self.assertEqual(head, '') #test if the head is extracted correctly
+        self.assertEqual(index_of_function, [(1,2)]) #test if the the index of function_name is assigned correctly
+        self.assertEqual(index_of_head,[(1,2)]) #the index of head should be [(1,2)]
+        self.assertEqual(body, ['body', 'body2']) #the body should be ['body', 'body2']
+        self.assertEqual(indexes_of_body, [(1,2), (1,2)]) #the index of body should be empty
+        self.assertEqual(index_of_excel_function, [(1,2)]) #the index of excel function should be [(1,2)]
+        self.assertEqual(excel_function, [':= "{{body:body2}}" + "tung"']) #the excel function list should be correct
+        self.assertEqual(other_info, []) #other information should be empty
+        self.assertEqual(index_of_other_info, []) #index of other information should be empty
+
+        #test for other information extraction:
+        function_name, head = extract_information(index_of_function, index_of_head, body, indexes_of_body,
+                        index_of_excel_function, excel_function, 'tung', 1, 2, other_info, index_of_other_info)
+        self.assertEqual(function_name, '') #test if the function_name is extracted correctly
+        self.assertEqual(head, '') #test if the head is extracted correctly
+        self.assertEqual(index_of_function, [(1,2)]) #test if the the index of function_name is assigned correctly
+        self.assertEqual(index_of_head,[(1,2)]) #the index of head should be [(1,2)]
+        self.assertEqual(body, ['body', 'body2']) #the body should be ['body', 'body2']
+        self.assertEqual(indexes_of_body, [(1,2), (1,2)]) #the index of body should be empty
+        self.assertEqual(index_of_excel_function, [(1,2)]) #the index of excel function should be [(1,2)]
+        self.assertEqual(excel_function, [':= "{{body:body2}}" + "tung"']) #the excel function list should be correct
+        self.assertEqual(other_info, ['tung']) #other information should be correct
+        self.assertEqual(index_of_other_info, [(1,2)]) #index of other information should be correct
