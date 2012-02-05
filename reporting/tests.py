19318b77ab4e1f8e6556c8026856b11696cdec1a
vuxuantung <toilatung90@gmail.com>
2012-02-05 10:18:53 +0700

    add unit tests

--- reporting/tests.py
+++ reporting/tests.py
@@ -6,11 +6,21 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from reporting.models import Upload, Spreadsheet_report
+from datetime import datetime
 
 
 class SimpleTest(TestCase):
+    def setUp(self):
+        self.upload = Upload.objects.create(filename = 'tung.xls', upload_time = datetime.now(), description = "tung", filestore = "tung.xls")
+        self.spreadsheet_report = Spreadsheet_report.objects.create(description = 'tung', created_time = datetime.now())
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+    def test_returned_name(self):
+        "Upload object should have name same as it's description"
+        self.assertEqual(str(self.upload), 'tung')
+        self.assertEqual(str(self.spreadsheet_report), 'tung')
---
+++ tests.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from report_tool.models import School, Class, Pupil
+from datetime import datetime
+
+
+class SimpleTest(TestCase):
+    def setUp(self):
+        self.school = School.objects.create(name = "tung")
+        self.class_for_test = Class.objects.create(name = "tung")
+        self.pupil = Pupil.objects.create(name = 'tung', school_id = self.school, class_id = self.class_for_test)
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+    def test_returned_name(self):
+        "Upload object should have name same as it's description"
+        self.assertEqual(str(self.school), 'tung')
+        self.assertEqual(str(self.class_for_test), 'tung')
+        self.assertEqual(str(self.pupil), 'tung')
+
+# -*- coding: utf-8 -*-
+from django.db import models
+
+class School(models.Model):
+    name    = models.CharField(max_length= 25)
+    def __unicode__(self):
+        return str(self.name)
+class Class(models.Model):
+    name    = models.CharField(max_length= 25)
+    def __unicode__(self):
+        return str(self.name)
+class Pupil(models.Model):
+    name    = models.CharField(max_length=25)
+    school_id  = models.ForeignKey(School)
+    class_id   =    models.ForeignKey(Class)
+    def __unicode__(self):
+        return str(self.name)
+
